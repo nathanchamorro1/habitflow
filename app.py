ce1d5be16ede2fd68bf271b4bd8f6e7885e27a82
nathanchamorro1 <160960078+nathanchamorro1@users.noreply.github.com>
2024-09-29 23:29:27 -0700

    reorganized files, updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder = "/Users/nathanchamorro/Desktop/habitflow_git/templates")
 
 @app.route("/")
 def home():
-    return "Hello, this is the homepage!"
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
